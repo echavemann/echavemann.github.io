967260c13974e57b46fc8314feef7a0adcd8a9a9
Ethan Havemann <echavemann@gmail.com>
2022-07-11 23:23:00 -0500

    helm of flame

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Footer, Blog, Possibility, Features, AboutMe, Header } from './containers';
 import { Resume, Brand, Navbar } from './components';
 import './app.css';
@@ -8,6 +9,9 @@ const App = () => {
 
   return (
     <div className="App">
+      <Helmet>
+        <title>Ethan Havemann</title>
+      </Helmet>
     <div className="gradient__bg">
       <Navbar />
       <Header />
