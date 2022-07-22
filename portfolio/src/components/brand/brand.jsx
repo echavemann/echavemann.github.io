6b74ec453d0291b64ed1de76beb8a7096d139c51
Ethan Havemann <echavemann@gmail.com>
2022-07-21 20:11:24 -0500

    refactore

--- portfolio/src/components/brand/brand.jsx
+++ portfolio/src/components/brand/brand.jsx
@@ -9,19 +9,19 @@ const Brand = () => {
             <h2>Experience with the following organizations:</h2>
         <div className='gpt3__brand section__padding'>
             <div>
-                <img src={amazon} alt = 'Amazon'/>
+                <a href = "https://www.amazon.com" target = "_blank" rel="noreferrer"><img src={amazon} alt = 'Amazon'/></a>
             </div>
             <div>
-                <img src={nu} alt = 'Northwestern University'/>
+                <a href = "https://www.northwestern.edu/" target = "_blank" rel="noreferrer"><img src={nu} alt = 'Northwestern University'/></a>
             </div>
             <div>
-                <img src={acorn} alt = 'Acorn Genetics'/>
+                <a href = "https://www.acorngenetics.com/" target = "_blank" rel="noreferrer"><img src={acorn} alt = 'Acorn Genetics'/></a>
             </div>
             <div>
-                <img src={hc} alt = 'Havemann Capital'/>
+                <a href = "https://echavemann.github.io/" target = "_blank" rel="noreferrer"><img src={hc} alt = 'Havemann Capital'/></a>
             </div>
             <div className='nufticon'>
-                <img src={nuft} alt = 'NUFintech'/>
+                <a href = "https://northwesternfintech.github.io/" target = "_blank" rel="noreferrer"><img src={nuft} alt = 'NUFintech'/></a>
             </div>
         </div>
         </div>
